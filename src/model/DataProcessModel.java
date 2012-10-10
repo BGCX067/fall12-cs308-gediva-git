029539e278355deda133ea1bf7c7c122adb4c7e9
Howard Chung <howard.chung@duke.edu>
2012-10-10 13:43:40 -0400

    renamed model

---
+++ src/model/DataProcessModel.java
@@ -0,0 +1,86 @@
+package model;
+
+import controller.Controller;
+import java.util.HashMap;
+import java.util.List;
+import resources.Constants;
+import visualizations.BarGraph;
+import visualizations.LineGraph;
+import visualizations.Visualization;
+
+
+/**
+ * Model for data processing.
+ * 
+ * @author Howard
+ * 
+ */
+public class DataProcessModel {
+    private FileOpener myFileOpener;
+    private HashMap<String, List<Double>> myAllValuesByRow;
+    private HashMap<String, List<Double>> myAllValuesByCol;
+
+    /**
+     * Creates a new model.
+     */
+    public DataProcessModel () {
+        myFileOpener = new FileOpener();
+    }
+
+    /**
+     * loads a file and fills HashMaps.
+     */
+    public void loadFile () {
+        myFileOpener.readFile();
+        myFileOpener.getFileParser().fillData();
+        myAllValuesByRow = myFileOpener.getFileParser().getValuesByRow();
+        myAllValuesByCol = myFileOpener.getFileParser().getValuesByCol();
+    }
+
+    /**
+     * 
+     * @param visType
+     * @param selectedRowOrCol
+     * @param contr
+     */
+    public void setVisualization (String visType, String selectedRowOrCol, Controller contr) {
+        /*
+         * for (String name : Constants.myNameMap.keySet()) {
+         * if (name.equals(visType)) {
+         * Constants.myNameMap.get(name).setValues(myAllValuesByCol.get(
+         * selectedRowOrCol),
+         * selectedRowOrCol, contr);
+         * }
+         * }
+         */
+
+        Visualization myVis = Constants.myNameMap.get(visType);
+        if (Constants.BAR_GRAPH.equals(visType)) {
+            myVis.setValues(myAllValuesByCol.get(selectedRowOrCol), selectedRowOrCol, contr);
+        }
+        else if (Constants.LINE_GRAPH.equals(visType)) {
+            myVis.setValues(myAllValuesByRow.get(selectedRowOrCol), selectedRowOrCol, contr);
+        }
+        myVis.visualize();
+
+    }
+
+    /**
+     * Get list of years.
+     * 
+     * @return
+     */
+
+    public String[] getAllYears () {
+        return myFileOpener.getFileParser().getAllYears();
+    }
+
+    /**
+     * Get list of countries.
+     * 
+     * @return
+     */
+    public String[] getAllCountries () {
+        return myFileOpener.getFileParser().getAllCountries();
+    }
+}
