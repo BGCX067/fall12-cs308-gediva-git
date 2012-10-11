fbdeb80e2b8469ac093571a53a81fd4955ef3358
Xi Du <duxi2323@gmail.com>
2012-10-11 16:04:36 -0400

    Fix part of the checkstyle issues in the Constants class

--- src/resources/Constants.java
+++ src/resources/Constants.java
@@ -29,24 +29,79 @@ public class Constants {
     public static final String CLEAR_BUTTON_TEXT = myResources.getString("ClearButtonText");
     public static final Dimension SELECTION_LIST_SIZE = new Dimension(75, 150);
     public static final Dimension MESSAGE_DISPLAY_SIZE = new Dimension(6, 30);
+    
+    
     // Visualizations
+    /**
+     * @param CHART_WIDTH the width of the chart
+     */
     public static final int CHART_WIDTH = 600;
+    /**
+     * @param CHART_HEIGHT the height of the chart
+     */
     public static final int CHART_HEIGHT = 400;
+    /**
+     * @param CHART_FONT_SIZE1 the font size used in LineGraph
+     */
     public static final int CHART_FONT_SIZE1 = 15;
+    /**
+     * @param CHART_FONT_SIZE2 the font size used in LineGraph
+     */
     public static final int CHART_FONT_SIZE2 = 10;
+    /**
+     * @param GAP_BETWEEN_CHART_AND_FRAME the gap used in the chart
+     */
     public static final int GAP_BETWEEN_CHART_AND_FRAME = 20;
+    /**
+     * @param Y_AXIS_NUBMER_OF_MARKS
+     */
     public static final int Y_AXIS_NUBMER_OF_MARKS = 10;
-    public static final Font CHART_TITLE_FONT = new Font("Book Antiqua", Font.BOLD, 15);
-    public static final Font CHART_LABEL_FONT = new Font("Book Antiqua", Font.PLAIN, 10);
+    /**
+     * @param CHART_TITLE_FONT title font size
+     */
+    public static final Font CHART_TITLE_FONT =
+            new Font("Book Antiqua", Font.BOLD, 15);
+    /**
+     * @param CHART_LABEL_FONT label font size
+     */
+    public static final Font CHART_LABEL_FONT =
+            new Font("Book Antiqua", Font.PLAIN, 10);
     // Line
+    /**
+     * @param LINE_COLOR line color
+     */
     public static final Color LINE_COLOR = Color.blue;
+    /**
+     * @param LINE_POINT_COLOR point color
+     */
     public static final Color LINE_POINT_COLOR = Color.red;
+    /**
+     * @param LINE_STROKE
+     */
     public static final Stroke LINE_STROKE = new BasicStroke(1f);
+    /**
+     * @param LINE_POINT_DIAMETER
+     */
     public static final int LINE_POINT_DIAMETER = 8;
-    public static final String LINE_BUTTON_ONCLICK_MESSAGE = myResources.getString("LineButtonOnClickMessage");
+    /**
+     * @param LINE_BUTTON_ONCLICK_MESSAGE message when clicking
+     */
+    public static final String LINE_BUTTON_ONCLICK_MESSAGE
+    = myResources.getString("LineButtonOnClickMessage");
+
     // Bar
+    /**
+     * @param BAR_FILL_COLOR color used in bar graph
+     */
     public static final Color BAR_FILL_COLOR = Color.blue;
+    /**
+     * @param BAR_BORDER_COLOR border color used in bar graph
+     */
     public static final Color BAR_BORDER_COLOR = Color.black;
-    public static final String BAR_BUTTON_ONCLICK_MESSAGE = myResources.getString("BarButtonOnClickMessage");
+    /**
+     * @param BAR_BUTTON_ONCLICK_MESSAGE message when clicking
+     */
+    public static final String BAR_BUTTON_ONCLICK_MESSAGE
+    = myResources.getString("BarButtonOnClickMessage");
 
 }
