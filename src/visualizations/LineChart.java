fcfab7b18874ca6dbe4e4eb4789110cf1cbf5ebd
Xi Du <duxi2323@gmail.com>
2012-10-12 12:45:28 -0400

    Split paint in LineChart class

--- src/visualizations/LineChart.java
+++ src/visualizations/LineChart.java
@@ -26,6 +26,18 @@ import static resources.Constants.LINE_BUTTON_ONCLICK_MESSAGE;
  */
 @SuppressWarnings("serial")
 public class LineChart extends Visualization {
+    /**
+     * @param myXScale
+     */
+    private final double myXScale =
+            ((double) getWidth() - 2 * GAP_BETWEEN_CHART_AND_FRAME)
+            / (getValues().size() - 1);
+    /**
+     * @param myYScale
+     */
+    private final double myYScale =
+            ((double) getHeight() - 2 * GAP_BETWEEN_CHART_AND_FRAME)
+            / (getMaxValue() - getMinValue());
 
     /**
      * Initializes Line Graph.
@@ -44,51 +56,22 @@ public class LineChart extends Visualization {
         final Graphics2D g2 = (Graphics2D) g;
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
                 RenderingHints.VALUE_ANTIALIAS_ON);
-        final double xScale =
-                ((double) getWidth() - 2 * GAP_BETWEEN_CHART_AND_FRAME)
-                / (getValues().size() - 1);
-        final double yScale =
-                ((double) getHeight() - 2 * GAP_BETWEEN_CHART_AND_FRAME)
-                / (getMaxValue() - getMinValue());
+
 
         final ArrayList<Point> graphPoints = new ArrayList<Point>();
         for (int i = 0; i < getValues().size(); i++) {
-            final int x1 = (int) (i * xScale + GAP_BETWEEN_CHART_AND_FRAME);
+            final int x1 = (int) (i * myXScale + GAP_BETWEEN_CHART_AND_FRAME);
             final int y1 =
-                    (int) ((getMaxValue() - getValues().get(i)) * yScale
+                    (int) ((getMaxValue() - getValues().get(i)) * myYScale
                             + GAP_BETWEEN_CHART_AND_FRAME);
             graphPoints.add(new Point(x1, y1));
         }
         // create x and y axes
-        g2.drawLine(GAP_BETWEEN_CHART_AND_FRAME, getHeight()
-                - GAP_BETWEEN_CHART_AND_FRAME,
-                    GAP_BETWEEN_CHART_AND_FRAME, GAP_BETWEEN_CHART_AND_FRAME);
-        g2.drawLine(GAP_BETWEEN_CHART_AND_FRAME, getHeight()
-                - GAP_BETWEEN_CHART_AND_FRAME,
-                    getWidth() - GAP_BETWEEN_CHART_AND_FRAME, getHeight()
-                    - GAP_BETWEEN_CHART_AND_FRAME);
+        createXYaxis(g2);
         // create hatch marks for y axis.
-        for (int i = 0; i < Y_AXIS_NUBMER_OF_MARKS; i++) {
-            final int x0 = GAP_BETWEEN_CHART_AND_FRAME;
-            final int x1 = LINE_POINT_DIAMETER + GAP_BETWEEN_CHART_AND_FRAME;
-            final int y0 =
-                    getHeight() - (((i + 1) * (getHeight()
-                            - GAP_BETWEEN_CHART_AND_FRAME * 2))
-                            / Y_AXIS_NUBMER_OF_MARKS
-                            + GAP_BETWEEN_CHART_AND_FRAME);
-            final int y1 = y0;
-            g2.drawLine(x0, y0, x1, y1);
-        }
+        drawMarkforY(g2);
         // and for x axis
-        for (int i = 0; i < getValues().size() - 1; i++) {
-            final int x0 =
-                    (i + 1) * (getWidth() - GAP_BETWEEN_CHART_AND_FRAME * 2)
-                    / (getValues().size() - 1) + GAP_BETWEEN_CHART_AND_FRAME;
-            final int x1 = x0;
-            final int y0 = getHeight() - GAP_BETWEEN_CHART_AND_FRAME;
-            final int y1 = y0 - LINE_POINT_DIAMETER;
-            g2.drawLine(x0, y0, x1, y1);
-        }
+        drawMarkforX(g2);
 
         final Stroke oldStroke = g2.getStroke();
         g2.setColor(LINE_COLOR);
@@ -108,6 +91,51 @@ public class LineChart extends Visualization {
             final int y = graphPoints.get(i).y - LINE_POINT_DIAMETER / 2;
             g2.fillOval(x, y, LINE_POINT_DIAMETER, LINE_POINT_DIAMETER);
         }
+    }
+    /**
+     * draw mark for X.
+     * @param g2
+     */
+    private void drawMarkforX (final Graphics2D g2) {
+        for (int i = 0; i < getValues().size() - 1; i++) {
+            final int x0 =
+                    (i + 1) * (getWidth() - GAP_BETWEEN_CHART_AND_FRAME * 2)
+                    / (getValues().size() - 1) + GAP_BETWEEN_CHART_AND_FRAME;
+            final int x1 = x0;
+            final int y0 = getHeight() - GAP_BETWEEN_CHART_AND_FRAME;
+            final int y1 = y0 - LINE_POINT_DIAMETER;
+            g2.drawLine(x0, y0, x1, y1);
+        }
+    }
+/**
+ * draw mark for Y.
+ * @param g2
+ */
+    private void drawMarkforY (final Graphics2D g2) {
+        for (int i = 0; i < Y_AXIS_NUBMER_OF_MARKS; i++) {
+            final int x0 = GAP_BETWEEN_CHART_AND_FRAME;
+            final int x1 = LINE_POINT_DIAMETER + GAP_BETWEEN_CHART_AND_FRAME;
+            final int y0 =
+                    getHeight() - (((i + 1) * (getHeight()
+                            - GAP_BETWEEN_CHART_AND_FRAME * 2))
+                            / Y_AXIS_NUBMER_OF_MARKS
+                            + GAP_BETWEEN_CHART_AND_FRAME);
+            final int y1 = y0;
+            g2.drawLine(x0, y0, x1, y1);
+        }
+    }
+/**
+ * create X and Y axis.
+ * @param g2
+ */
+    private void createXYaxis (final Graphics2D g2) {
+        g2.drawLine(GAP_BETWEEN_CHART_AND_FRAME, getHeight()
+                - GAP_BETWEEN_CHART_AND_FRAME,
+                    GAP_BETWEEN_CHART_AND_FRAME, GAP_BETWEEN_CHART_AND_FRAME);
+        g2.drawLine(GAP_BETWEEN_CHART_AND_FRAME, getHeight()
+                - GAP_BETWEEN_CHART_AND_FRAME,
+                    getWidth() - GAP_BETWEEN_CHART_AND_FRAME, getHeight()
+                    - GAP_BETWEEN_CHART_AND_FRAME);
     }
 
     /**
